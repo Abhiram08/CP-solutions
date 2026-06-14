5678a65f2d64e75655f04d3723c0a4d49d77de71
Abhiram Datt <abhiramdatt08@gmail.com>
2026-06-15 01:49:01 +0530

    Completed Problem 2035B from Codeforces

--- CF-Solutions/Problem2035B/main.cpp
+++ CF-Solutions/Problem2035B/main.cpp
@@ -1,44 +1,37 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-    int t{};
-    std::cin >> t;
-    for (int i = 1; i <= t; i++)
-    {
-        long long n{};
-        std::cin >> n;
-        if (n >= 4)
-        {
-            std::string num = "51";
-            if (n % 2 == 0)
-            {
-                for (long long i = 1; i <= (n - 4) / 2; i++)
-                {
-                    num.insert(0, "50");
-                }
-                long long x{std::stoi(num)};
-                std::cout << static_cast<long long>(66 * x) << "\n";
-            }
-            else
-            {
-                num.insert(0, "5");
-                for (long long i = 1; i <= (n - 4) / 2; i++)
-                {
-                    num.insert(0, "50");
-                }
-                long long x{std::stoi(num)};
-                std::cout << static_cast<long long>(66 * x) << "\n";
-            }
-        }
-        else if (n == 2)
-        {
-            std::cout << "66\n";
-        }
-        else
-        {
-            std::cout << "-1\n";
-        }
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    // Impossible cases
+    if (n == 1 || n == 3) {
+        cout << -1 << "\n";
+        return;
     }
+
+    // Even length case
+    if (n % 2 == 0) {
+        cout << string(n - 2, '3') << "66\n";
+    } 
+    // Odd length case
+    else {
+        cout << string(n - 5, '3') << "36366\n";
+    }
+}
+
+int main() {
+    // Fast I/O for competitive programming
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
