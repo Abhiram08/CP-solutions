b61ec6165897f4fbaa13497f6a7f66989b9b195a
Abhiram Datt <abhiramdatt08@gmail.com>
2026-06-24 02:08:39 +0530

    Completed Problem 2203B from Codeforces

---
+++ CF-Solutions/Problem2203B/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int sum = 0;
+        vector<int> a;
+        for (int i = 0; i < int(s.size()); ++i)
+        {
+            int x = s[i] - '0';
+            sum += x;
+            a.push_back(x - (i == 0));
+        }
+        sort(a.begin(), a.end());
+        int ans = 0;
+        while (sum > 9)
+        {
+            sum -= a.back();
+            a.pop_back();
+            ++ans;
+        }
+        cout << ans << '\n';
+    }
+}
